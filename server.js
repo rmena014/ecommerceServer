6577db41166c64e8ef75c1f72e1957fd3ec6e874
rmena014 <rmena014@gmail.com>
2023-01-03 01:42:07 -0500

    Fixed mmysql file path

--- server.js
+++ server.js
@@ -4,14 +4,9 @@ const cors = require("cors");
 const path = require("path");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
-// const mysqlConfig = require("../mysql-config");
+const mysqlConfig = require("./mysql-config");
 
-const db = mysql.createConnection({
-  host: "ecommercev2.cqwpxjxkqcuo.us-east-1.rds.amazonaws.com",
-  user: "admin",
-  password: "Richjenn014",
-  database: "ecommerce",
-});
+const db = mysql.createConnection(mysqlConfig);
 
 const handleQueryResult = (err, results, res) => {
   if (err) {
